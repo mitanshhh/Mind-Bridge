2a8bd07198bd66726460d7c374bf070083878f88
mitanshhh <mitansh.jadhav2007@gmail.com>
2026-01-27 13:08:05 +0530

    Made changes for Mobile in Html

--- main_chat.py
+++ main_chat.py
@@ -130,7 +130,7 @@ st.markdown("""
     background-color: #0e1117;
     z-index: 1000;
     box-shadow: 0 -4px 6px -1px rgba(0, 0, 0, 0.1);
-    bottom: 25px;
+    bottom: 45px;
 }
 .st-emotion-cache-8atqhb {
     width: 33%;
